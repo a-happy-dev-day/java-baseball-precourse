43038974d3953bddb709375f1b87148f7129f8c9
JohnPrk <uo3641493@gmail.com>
2022-10-06 19:00:12 +0900

    refactor : 인스턴스 변수를 지역 변수로 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,16 +1,17 @@
 package baseball;
 
 public class Game {
-    private User user = new User();
-    RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-    private CompareSystem compareSystem = new CompareSystem();
-    private Numbers random_numbers;
-    private Numbers user_numbers;
     static final String RESTART = "1";
     static final String EXIT = "2";
 
     public void play() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        CompareSystem compareSystem = new CompareSystem();
+        User user = new User();
+        Numbers random_numbers;
+        Numbers user_numbers;
         boolean keepGoing = false;
+
         while(!keepGoing) {
             System.out.print("서로 다른 3자리 숫자를 입력해주세요. ");
             random_numbers = randomNumberGenerator.createRandomNumbers();
@@ -21,6 +22,8 @@ public class Game {
     }
 
     public boolean restartOrExit() {
+        User user = new User();
+
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String checkResult = user.inputRestartOrExit();
         if(!(checkResult.equals(RESTART) || checkResult.equals(EXIT))) {
