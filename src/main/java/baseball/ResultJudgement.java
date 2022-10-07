f2dcaafd1bae988fa57ac2f3d01db6d81ebf2f78
JT <38105420+wjdxor@users.noreply.github.com>
2022-10-07 18:25:35 +0900

    fix: Result 출력문 수정

--- src/main/java/baseball/ResultJudgement.java
+++ src/main/java/baseball/ResultJudgement.java
@@ -16,8 +16,18 @@ public class ResultJudgement {
                 if (i == j) continue;
                 if (computer.get(i).equals(user.get(j))) ball++;
             }
+
         }
-        System.out.println(ball + "볼" + strike + "스트라이크");
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+
         if (strike == 3) {
             return true;
         }
