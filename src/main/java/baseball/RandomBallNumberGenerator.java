3bbfa11314b4c9112ba98be7f91d0ebe0200cac4
JohnPrk <uo3641493@gmail.com>
2022-10-17 21:44:17 +0900

    refactor : 클래스명 변경(RandomNumberGenerator)

--- src/main/java/baseball/RandomBallNumberGenerator.java
+++ src/main/java/baseball/RandomBallNumberGenerator.java
@@ -6,23 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class RandomNumberGenerator {
+public class RandomBallNumberGenerator {
 
     private static final int MAX_NUMBER = 9;
     private static final int MIN_NUMBER = 1;
     private static final int NUMBER_SIZE = 3;
 
-    public Numbers createRandomNumbers() {
+    public BallNumbers createRandomNumbers() {
         List<Integer> number_list = new ArrayList<>();
-        List<Number> number_object = new ArrayList<>();
+        List<BallNumber> number_object = new ArrayList<>();
         while (number_list.size() < NUMBER_SIZE) {
             int num = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if (!number_list.contains(num)) {
                 number_list.add(num);
-                number_object.add(new Number(num));
+                number_object.add(new BallNumber(num));
             }
         }
 
-        return new Numbers(number_object);
+        return new BallNumbers(number_object);
     }
 }
